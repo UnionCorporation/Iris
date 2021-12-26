d7641a23d305a303fb7f738bb7ee29123a07a953
UnionCorporation <43969388+UnionCorporation@users.noreply.github.com>
2021-12-26 13:38:19 +0100

    animation channel finished

--- tools/encoder/src/AnimationChannel.cpp
+++ tools/encoder/src/AnimationChannel.cpp
@@ -131,8 +131,102 @@ namespace gameplay
             size_t i = 1;
             for (i = 1; i < _keytimes.size(); ++i)
             {
-                std::vector<float>::iterator
+                std::vector<float>::iterator start = _keyValues.begin() + i * propSize;
+                std::vector<float>::iterator end = start + propSize - 1;
+
+                if (!equal(prevStart, prevEnd, start) | | i = _keytimes.size() - 1)
+                {
+                    if (i - prevIndex > 2)
+                    {
+                        deleteRange(prevIndex+1, i, propSize);
+                        i = prevIndex;
+                        prevStart = _keyValues.begin() + i * propSize;
+                        prevEnd = prevStart + propSize - 1;
+                    }
+                    else 
+                    {
+                        prevStart = start;
+                        prevEnd = end;
+                        prevIndex = i;
+                    }
+                }
             }
+            if (i - 1 - prevIndex >= 2)
+            {
+                deleteRange(prevIndex+1, i, propSize);
+            }
+        }
+        LOG(3, "Removed duplicate frames", startCount - _keytimes.size());
+    }
+    unsigned int AnimationChannel::getInterpolationType(const char* str)
+    {
+        unsigned int value = 0;
+        switch (str[0])
+        {
+            case 'L':
+            if (strcmp(str, "LINEAR") == 0)
+            {
+                value = AnimationChannel::LINEAR;
+            }
+            break;
+            
+            case 'B':
+            if (strcmp(str, "BEZIER") == 0)
+            {
+                value = AnimationChannel::BEZIER;
+            }
+            else if (strcmp(str, "BSPLINE") == 0)
+            {
+                value = AnimationChannel::BSPLINE;
+            }
+            break;
+
+            case 'C':
+            if (strcmp(str, "CARDINAL") == 0)
+            {
+                value = AnimationChannel::CARDINAL;
+            }
+            break;
+            
+            case 'H':
+            if (strcmp(str, "HERMITE") == 0)
+            {
+                value = AnimationChannel::HERMIT;
+            }
+            break;
+            
+            case 'S':
+            if (strcmp(str, "STEP") == 0)
+            {
+                value = AnimationChannel::STEP;
+            }
+            break;
+        default:
+            break;
+
+        }
+        return value;
+    }
+
+    void AnimationChannel::deleteRange(size_t begin, size_t end, size_t propSize)
+    {
+        assert(end > begin);
+
+        LOG(4, "             delete %lu to %lu\n", begin, end - 1);
+
+        std::vector<float>::iterator a = _keyValues.begin() + begin * propSize;
+        std::vector<float>::iterator b = _keyValues.begin() + end * propSize;
+        _keyValues.erase(a, b);
+
+        a = _keytimes.begin() + begin;
+        b = _keytimes.begin() + end;
+        _keytimes.erase(a, b);
+
+        if (_interpolations.size() > 1)
+        {
+            std::vector<unsigned int>::iterator a = _interpolations.begin() + begin;
+            std::vector<unsigned int>::iterator b = _interpolations.begin() + end * propSize;
+            _interpolations.erase(a, b);
         }
     }
         
